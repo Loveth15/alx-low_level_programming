47d3bc3c8380e9429ca380dcd7ea799b0cff6b69
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-06 15:14:58 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,9 +3,24 @@
 /**
  * main - Entry point
  *
+ * Description: Prints alphabets in lower case
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	putchar(
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+
+	{
+		putchar (c);
+		c = c + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
