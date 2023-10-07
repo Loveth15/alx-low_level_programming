fba12b194c61e54de4396b294493d88a5c31667d
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-07 12:50:49 -0700

    print lowercase alphabets

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints lower case alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	printf("%d", a);
+	}
+
+	printf("\n");
+
+	return (0);
+}
