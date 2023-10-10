eefc58b394b4da8e80d618f82783a241c968eaa3
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-10 11:05:24 -0700

    repeat lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+
+	for (j = 1, j < 10, j++)
+	{
+		while (i <= 'z')
+		{
+			_putchar(c);
+			c = c + 1;
+		}
+	_putchar('\n');
+}
