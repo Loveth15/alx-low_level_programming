4a8feb46402e9cacad0fc58cb44e55be1c3a4bf7
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-10 10:19:31 -0700

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar (c);
+		c = c + 1;
+	}
+
+	_putchar('\n');
+return (0);
+}
