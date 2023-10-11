a62e5f75c9bf9acb40adc8540e59476637d5e8f8
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-11 07:37:47 -0700

    Checking for lowercase alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - this program checks for lowercase letters
+ *
+ * @c: c represents lowercase alphabets.
+ *
+ * Return: 1 when c is lowercase and 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 99 && <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
