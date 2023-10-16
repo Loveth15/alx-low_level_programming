78c9c1fe543d6af367dc43f3ee729ad8151f7354
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-16 13:59:16 -0700

    Printing a string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - A program that prints a string, followed by a new line.
+ *
+ * @str: string to be printed.
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	_puts(str);
+
+	_puts("\n");
+
+	return (0);
+}
