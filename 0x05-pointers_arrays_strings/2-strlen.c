b5aaa6ad03c26b98f57751a2f68f53cdad33cc55
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-16 10:35:28 -0700

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-echo $PATH | tr -s ":" "\n" | wc -|
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - This program checks the length of a string
+ *
+ * @s: String
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	size _strlen(const char *s);
+}
