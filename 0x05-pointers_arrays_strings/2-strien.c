ca46279548196e763cfb88330f2588d0a826aa4a
Loveth Ogo <ogoloveth73@gmail.com>
2023-10-16 10:22:36 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - This program returns the length of a string
+ *
+ * @s: A string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	size _strlen(const char *s);
+}
