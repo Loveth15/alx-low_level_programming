03aedb323b5b53a7a45abedaafd2b6678d4ff6e6
Loveth_Ogo <ogoloveth73@gmail.com>
2023-11-01 02:25:29 +0100

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,7 +5,6 @@
  * str_concat - This function concatenates two strings
  * @s1: string to concatenate
  * @s2: other string to concatenate
- *
  * Return: pointer to the new string created (Success), or NULL (Error)
 */
 
@@ -15,9 +14,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-	{
 		len1++;
-	}
+
 	while (s2 && s2[len2])
 	{
 		len2++;
