8d532a1340f43158d79ddcdbb3124dd0f4e8f4f0
Loveth_Ogo <ogoloveth73@gmail.com>
2023-11-01 03:09:11 +0100

    splits strings

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,8 +31,7 @@ int count_word(char *s)
 /**
  * **strtow - This function splits a string into words
  * @str: string to split
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: pointer to an array of strings (Success) or NULL
 */
 
 char **strtow(char *str)
