bdb123efe3704015b0d893f482c75271fe924e8a
Loveth_Ogo <ogoloveth73@gmail.com>
2023-11-22 12:59:17 +0100

    Correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,18 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	if(!head)
+	{
+		return (NULL);
+	}
+
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
+	{
 		return (NULL);
+	}
 
 	new->n = n;
 	new->next = *head;
